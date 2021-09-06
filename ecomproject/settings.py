a12f9c39c825c7663d393f40450350fdd35d4e1d
CAPECOBRA <786amitsingh86@gmail.com>
2021-09-07 00:07:36 +0530

    commit

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+BASE_DIR=Path(__file__).resolve().parent.parent
+STATIC_ROOT=BASE_DIR/'staticfiles'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
